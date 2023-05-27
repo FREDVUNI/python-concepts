b35f813290e13c08d0677020411d8474858aa0ba
Fred Vuni <41730664+FREDVUNI@users.noreply.github.com>
2023-05-27 13:49:00 +0300

    Create problem-2.py

---
+++ problem-2.py
@@ -0,0 +1,72 @@
+class GoCardAccount:
+    def __init__(self, initial_balance):
+        self.balance = initial_balance
+        self.transactions = [("Initial balance", initial_balance)]
+
+    def record_ride(self, amount):
+        self.balance -= amount
+        self.transactions.append(("Ride", amount, self.balance))
+
+    def record_top_up(self, amount):
+        self.balance += amount
+        self.transactions.append(("Top up", amount, self.balance))
+
+    def get_balance(self):
+        return self.balance
+
+    def generate_statement(self):
+        statement = "Statement:\n"
+        statement += "{:<20}{:<12}{:<12}\n".format("event", "amount ($)", "balance ($)")
+        for transaction in self.transactions:
+            if len(transaction) == 3:
+                event, amount, balance = transaction
+                statement += "{:<20}{:<12.2f}{:<12.2f}\n".format(event, amount, balance)
+            else:
+                event, balance = transaction
+                statement += "{:<20}{:<12}{:<12.2f}\n".format(event, "", balance)
+        return statement
+
+
+def process_command(command, account, bad_commands):
+    parts = command.split()
+    if len(parts) == 0:
+        return
+
+    action = parts[0]
+    if action == "q":
+        return False
+    elif action == "b":
+        print(f"Balance = ${account.get_balance():.2f}")
+    elif action == "r" and len(parts) == 2 and parts[1][0].isdigit():
+        amount_str = parts[1]
+        try:
+            amount = float(amount_str)
+            account.record_ride(amount)
+        except ValueError:
+            if len(bad_commands) < 3:
+                bad_commands.append(command)
+    elif action == "t" and len(parts) == 2 and parts[1][0].isdigit():
+        amount_str = parts[1]
+        try:
+            amount = float(amount_str)
+            account.record_top_up(amount)
+        except ValueError:
+            if len(bad_commands) < 3:
+                bad_commands.append(command)
+    else:
+        if len(bad_commands) < 3 and not (action.startswith("r") or action.startswith("t")):
+            bad_commands.append(command)
+
+    return True
+
+
+initial_balance = float(input("Creating account. Input initial balance: "))
+account = GoCardAccount(initial_balance)
+bad_commands = []
+
+command = input("? ")
+while process_command(command, account, bad_commands):
+    command = input("? ")
+
+print(account.generate_statement())
+print("Final balance\t\t\t{:.2f}".format(account.get_balance()))
